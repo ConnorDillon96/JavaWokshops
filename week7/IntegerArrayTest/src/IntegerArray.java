c6973e24347cf41f7ef89ecc5814500063c1d37c
Connor Dillon <connordillon96@gmail.com>
2020-03-22 14:59:47 +0000

    recursion

--- week7/IntegerArrayTest/src/IntegerArray.java
+++ week7/IntegerArrayTest/src/IntegerArray.java
@@ -1,6 +1,7 @@
+
 public class IntegerArray {
     private Integer[] integers;
-    
+
     public IntegerArray(Integer[] integers){
         this.integers=new Integer[integers.length];
         // copy the array parameter to the attribute
@@ -10,17 +11,46 @@ public class IntegerArray {
     
     public String toString(){
         // add the call to the recursive version here
-        
+        String answer = "";
+        int pos = 0;
+        return toStringHelper(answer, pos); 
+
     }
     
     // add the recursive version here
+    public String toStringHelper(String answer, int pos){
+        if(pos==integers.length){
+            return answer;
+        }else{
+            answer += integers[pos];
+            pos++;
+        }
+
+        return toStringHelper(answer, pos);
+
+    }
     
     
     public Integer getTotal(){
         // add the call to the recursive version here
+        int total = 0;
+        int index = 0;
+        return getTotoalHelper(total, index);
         
     }
     
     // add the recursive version here
+    public Integer getTotoalHelper(int total, int index){
+        if(index == integers.length){
+            return total;
+        }else{
+            total += integers[index];
+            index++;
+        }
+        return getTotoalHelper(total, index);
+
+
+    }
 
 }
+
